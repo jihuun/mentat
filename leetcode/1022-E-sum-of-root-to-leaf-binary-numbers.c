eea33825e32f0c4ce5302cfd72cbc2ac27947e90
Ji-Hun Kim <jihuun.k@gmail.com>
2022-05-31 21:23:53 +0900

    update mentat training
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

---
+++ leetcode/1022-E-sum-of-root-to-leaf-binary-numbers.c
@@ -0,0 +1,29 @@
+/*
+ * https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int recur(struct TreeNode* root, int val)
+{
+    int sum = 0; 
+    
+    if (root->left == NULL && root->right == NULL)
+        return val;
+    if (root->left)
+        sum += recur(root->left, root->left->val + val * 2);
+    if (root->right)
+        sum += recur(root->right, root->right->val + val * 2);
+    return sum;
+}
+
+int sumRootToLeaf(struct TreeNode* root){
+    return recur(root, root->val);
+}

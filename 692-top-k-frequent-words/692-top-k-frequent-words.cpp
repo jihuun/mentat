18c1d62c4ee29b5660f58ec1d4cb56b8d824065f
JI-HUN KIM <jihuun.k@gmail.com>
2022-08-07 18:03:58 +0900

    Time: 28 ms (21.93%), Space: 12.8 MB (34.13%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,31 @@
+class myCompare {
+    public:
+    bool operator() (const pair<string, int> &p1, const pair<string, int> &p2) {
+        if (p1.second == p2.second)
+            return p1.first > p2.first;
+        return p1.second < p2.second;
+    }
+};
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        /* generate hash map */
+        unordered_map<string, int> freq;
+        for (string w: words)
+            freq[w]++;
+        
+        /* push string and freq value to heap */
+        priority_queue<pair<string, int>, vector<pair<string, int> >, myCompare> pq;
+        for (auto it: freq)
+            pq.push({it.first, it.second});
+        
+        /* pop k times */
+        vector<string> ret;
+        for (int i = 0; i < k; i++) {
+            ret.push_back(pq.top().first);
+            pq.pop();
+        }
+        return ret;
+    }
+};

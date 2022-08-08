0edcf699786b339dc7939a6ea3f7f03f34e7e575
JI-HUN KIM <jihuun.k@gmail.com>
2022-08-08 12:39:34 +0900

    Time: 91 ms (57.47%), Space: 13.1 MB (52.84%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    int power_of_num(int val, int *dp) {
+    int dp[1000001];
+    int power_of_num(int val) {
         if (val == 1)
             return 0;
         if (dp[val])
             return dp[val];
         if (val % 2 == 0)
-            dp[val] = power_of_num(val / 2, dp) + 1;
+            dp[val] = power_of_num(val / 2) + 1;
         else
-            dp[val] = power_of_num(val * 3 + 1, dp) + 1;
+            dp[val] = power_of_num(val * 3 + 1) + 1;
         return dp[val];
     }
 public:
     int getKth(int lo, int hi, int k) {
         int ret = 0;
-        int dp[1000001] = {0};
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
         for (int i = lo; i <= hi; i++)
-            pq.push(make_pair(power_of_num(i, dp), i));
+            pq.push(make_pair(power_of_num(i), i));
         while (k--) {
             ret = pq.top().second;
             pq.pop();

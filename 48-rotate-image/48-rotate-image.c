b5d24a9769eea2bcd39035bcf0e948141c8f59e4
JI-HUN KIM <jihuun.k@gmail.com>
2022-08-09 21:27:09 +0900

    Time: 7 ms (41.18%), Space: 6.3 MB (69.96%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.c
@@ -0,0 +1,14 @@
+
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize){
+    int n = matrixSize;
+    for (int i = 0; i < (n + 1) / 2; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = tmp;
+        }
+    } 
+}

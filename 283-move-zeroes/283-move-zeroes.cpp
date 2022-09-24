720e97b20ff4ca6692695ee015a35a13d4604409
JI-HUN KIM <jihuun.k@gmail.com>
2022-09-24 16:14:40 +0900

    Time: 49 ms (36.02%), Space: 19.3 MB (13.83%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -17,14 +17,18 @@ z : increase at
 nz: increase at
 */
 class Solution {
+    void swap(vector<int>& nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
 public:
     void moveZeroes(vector<int>& nums) {
         int retcnt = 0;
+        int last_zero = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0)
-                nums[retcnt++] = nums[i];
+                swap(nums, i, last_zero++);
         }
-        for (int i = retcnt; i < nums.size(); i++)
-            nums[i] = 0;
     }
 };

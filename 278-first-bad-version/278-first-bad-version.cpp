0e0c09166c1133307f190cea47b533eec0b8cd89
JI-HUN KIM <jihuun.k@gmail.com>
2022-09-25 14:09:31 +0900

    Time: 3 ms (37.64%), Space: 5.9 MB (67.75%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -8,10 +8,10 @@ public:
         int left = 1, right = n, mid = 0;
         while (left < right) {
             mid = left + (right - left) / 2;
-            if (isBadVersion(mid) == false)
-                left = mid + 1;
-            else 
+            if (isBadVersion(mid) == true)
                 right = mid;
+            else 
+                left = mid + 1;
         }
         return left;
     }

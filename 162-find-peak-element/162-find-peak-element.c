2e7907b33e1f0740ce59870ee77f3e7ba4eb1b56
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-07 11:43:09 +0900

    Time: 10 ms (18.01%), Space: 5.7 MB (70.12%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.c
+++ 162-find-peak-element/162-find-peak-element.c
@@ -28,11 +28,16 @@ int findPeakElement(int* nums, int numsSize){
             right = mid - 1;
         } else if (mid + 1 < numsSize && nums[mid] < nums[mid + 1]) {
             left = mid + 1;  
+        } else { 
+            return mid;
+        }   
+        /*
         } else if (((mid - 1 >=0 && mid + 1 < numsSize) && (nums[mid - 1] < nums[mid] && nums[mid > nums[mid - 1]])) ||
                      mid == 0 && nums[mid + 1] < nums[mid] || 
                      mid == numsSize - 1 && nums[mid] > nums[mid - 1]) {
             return mid;
         }
+        */
     }
     return left;
 }

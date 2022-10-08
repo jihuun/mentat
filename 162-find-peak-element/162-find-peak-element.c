519fc1092ccda0aee54df24e3973c7da11ec6d23
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-08 20:02:05 +0900

    Time: 6 ms (59.77%), Space: 6 MB (33.46%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.c
+++ 162-find-peak-element/162-find-peak-element.c
@@ -16,28 +16,14 @@ _|   _ | _
 */
 int findPeakElement(int* nums, int numsSize){
     int left = 0, right = numsSize - 1, mid = 0;
-    // check input valid range : 
-    if (numsSize == 0) {
-        fprintf(stderr, "invalid input\n");
-        return 1;
-    }
         
     while (left < right) {
         mid = left + (right - left) / 2; 
-        if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
-            right = mid - 1;
-        } else if (mid + 1 < numsSize && nums[mid] < nums[mid + 1]) {
-            left = mid + 1;  
-        } else { 
-            return mid;
-        }   
-        /*
-        } else if (((mid - 1 >=0 && mid + 1 < numsSize) && (nums[mid - 1] < nums[mid] && nums[mid > nums[mid - 1]])) ||
-                     mid == 0 && nums[mid + 1] < nums[mid] || 
-                     mid == numsSize - 1 && nums[mid] > nums[mid - 1]) {
-            return mid;
+        if (nums[mid] > nums[mid+1]) {  // case dec
+            right = mid;
+        } else {
+            left = mid + 1;
         }
-        */
     }
     return left;
 }

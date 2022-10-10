dd7868ccac01ad867c788406b49ddda50c593d1f
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-10 22:00:35 +0900

    Time: 813 ms (16.52%), Space: 51.2 MB (9.15%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -4,21 +4,29 @@ T T F F F T F F F
 1 0 2
 */
 class Solution {
+    vector<int> mem;
+    int last_idx;
+    bool check_reach(vector<int>& nums, int cur, int end) {
+        if (mem[cur] != -1)
+            return mem[cur];
+        if (cur == last_idx)
+            return true;
+        if (end > last_idx)
+            end = last_idx;
+        for (int i = cur + 1; i <= end; i++) {
+            if (mem[i] == -1)
+                mem[i] = check_reach(nums, i, i + nums[i]);
+            if (mem[i] == true)
+                return true;
+        }
+        return false;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        int nsize = nums.size();
-        if (nsize == 1) return true;
-        vector<bool> mem(nsize, false);
-        for (int i = nsize - 2; i >= 0; i--) {
-            for (int j = i; j <= i + nums[i]; j++) {
-                if (j == nsize - 1 || mem[j] == true) {
-                    mem[i] = true;
-                    if (i == 0)
-                        return true;
-                    break;
-                }
-            }
-        }    
-        return false;
+        last_idx = nums.size() - 1;
+        if (last_idx == 0) return true;
+        mem = vector<int>(nums.size(), -1);
+        
+        return check_reach(nums, 0, nums[0]);
     }
 };

f40258f5a0bf31e69af6d0b1b77b3d37ab27c395
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-27 08:19:58 +0900

    Time: 219 ms (65.13%), Space: 79.8 MB (31.34%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,28 +3,32 @@ class Solution {
     vector<string> tmp;
     
     bool is_palindrome(string s) {
-        int ssize = s.length();
-        for (int i = 0; i < ssize / 2; i++) {
-            if (s[i] != s[ssize - i - 1])
+        int slen = s.length();
+        
+        for (int i = 0; i < slen / 2; i++) {
+            if (s[i] != s[slen - i - 1])
                 return false;
         }
         return true;
     }
+    
     void recur(string s, int cur) {
-        if (cur >= s.length()) {
+        int ssize = s.length();
+        if (cur >= ssize) {
             ret.push_back(tmp);
             return;
         }
         
-        for (int i = cur; i < s.length(); i++) {
-            string tgt = s.substr(cur, i-cur+1);
-            if (!is_palindrome(tgt))
+        for (int i = cur; i < ssize; i++) {
+            string sub = s.substr(cur, i - cur + 1);
+            if (!is_palindrome(sub))
                 continue;
-            tmp.push_back(tgt);
-            recur(s, i+1);
+            tmp.push_back(sub);
+            recur(s, i + 1);
             tmp.pop_back();
-        }
+        }        
     }
+    
 public:
     vector<vector<string>> partition(string s) {
         recur(s, 0);

d3bdf3d3bc5212a0026052f2d22a747bebad9f20
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-29 08:34:43 +0900

    Time: 989 ms (34.59%), Space: 89.7 MB (81.73%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,36 +2,32 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int cur = 0;
-        int next = 1;
-        int min_rm = 0;
-        if (intervals.size() == 1)
-            return min_rm;
+        int nex = 1;
+        int ret = 0;
         sort(intervals.begin(), intervals.end());
         
-        /*
-        case 1 
-          ---- ---
-        case 2 : remove cur
-          --------x
-             ---
-        case 3 : remove next
-          -----
-            ------x
-        */
-        while (next < intervals.size()) {
-            // if case 1
-            if (intervals[cur][1] <= intervals[next][0]) {
-                cur = next; // <- cur will be next , not cur++
-                next++;
-            } else if (intervals[cur][1] >= intervals[next][1]) { // case 2
-                min_rm++;
-                cur = next; // <- cur will be next
-                next++;
-            } else { // case 3 
-                min_rm++;
-                next++;
+        while (nex != intervals.size()) {
+            if (intervals[cur][1] <= intervals[nex][0]) {
+                cur = nex;
+            } else if (intervals[cur][1] >= intervals[nex][1]) {
+                cur = nex;
+                ret++;
+            } else {
+                ret++;
             }
+            nex++;
         }
-        return min_rm;
+        return ret;
+/*
+
+---  ---
+
+cur  ----
+nex   --
+
+cur ---
+nex   ---
+
+*/
     }
 };

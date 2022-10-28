b32fc23e7123ecee848a95106696b2ec8db02345
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-28 09:25:09 +0900

    Time: 11 ms (10.90%), Space: 8 MB (10.14%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,24 +1,22 @@
 class Solution {
-public:
     unordered_map<int, int> mem;
+    int ret;
+public:
     int recur(string s, int pos) {
         if (mem.find(pos) != mem.end())
             return mem[pos];
         if (pos == s.length())
             return 1;
-        if (pos > s.length())
-            return 0;
-        if (s[pos] == '0')
+        if (pos > s.length() || s[pos] == '0')
             return 0;
         
-        int ret = 0;
-        ret += recur(s, pos + 1);
+        int ret = recur(s, pos + 1);
         if (stoi(s.substr(pos, 2)) <= 26)
             ret += recur(s, pos + 2);
         mem[pos] = ret;
         return ret;
     }
     int numDecodings(string s) {
-        return recur(s, 0);
+        return  recur(s, 0);
     }
 };

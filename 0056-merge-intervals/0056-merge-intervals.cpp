694652ebabe54f510195b177340f5fe3e8e16f06
JI-HUN KIM <jihuun.k@gmail.com>
2022-10-30 10:03:54 +0900

    Time: 89 ms (33.51%), Space: 19 MB (70.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ret;
-        int ret_idx = 0;
-        int cmp_idx = 1;
+        int isize = intervals.size();
+        int retidx = 0;
         
-        std::sort(intervals.begin(), intervals.end());
-        vector<int> tmp;
-        tmp.push_back(intervals[0][0]);
-        tmp.push_back(intervals[0][1]);
-        ret.push_back(tmp);
-        
-        for (int i = cmp_idx; i < intervals.size(); i++) {
-            if (ret[ret_idx][1] >= intervals[i][0]) { // must combine to ret[ret_idx]
-                ret[ret_idx][1] = std::max(ret[ret_idx][1], intervals[i][1]);
+        sort(intervals.begin(), intervals.end());
+        ret.push_back(intervals[0]);
+        for (int i = 1; i < isize; i++) {
+            // check ret[retidx] and interval[i]
+            if (ret[retidx][1] >= intervals[i][0]) {
+                ret[retidx][1] = max(ret[retidx][1], intervals[i][1]);
             } else {
-                vector<int> tmp;
-                tmp.push_back(intervals[i][0]);
-                tmp.push_back(intervals[i][1]);
-                ret.push_back(tmp);
-                ret_idx++;
+                ret.push_back(intervals[i]);
+                retidx++;
             }
         }
         return ret;

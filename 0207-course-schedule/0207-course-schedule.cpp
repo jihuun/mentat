79e4d77a0875fdb661c62cde87ba1e7012b220b4
JI-HUN KIM <jihuun.k@gmail.com>
2022-11-06 16:34:00 +0900

    Time: 49 ms (47.36%), Space: 14 MB (53.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,35 @@
 class Solution {
     vector<vector<int>> adj;
     bool is_cycle(vector<int> &vis, int cur) {
-        if (vis[cur] == 1) // if cycle, 
-            return true; 
+        /*
+        vis 0: not visited
+        vis 1: visited
+        vis 2: no need to visit
+        */
+        if (vis[cur] == 1)
+            return true;
         if (vis[cur] == 0) {
-            //cout << cur << " ";
             vis[cur] = 1;
             for (int i = 0; i < adj[cur].size(); i++) {
                 if (is_cycle(vis, adj[cur][i]))
                     return true;
             }
         }
-        vis[cur] = 2; // if no cycle, visited node will be 2
+        vis[cur] = 2;
         return false;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         adj = vector<vector<int>>(numCourses);
-        vector<int> visited(numCourses, 0);
+        vector<int> visites(numCourses, 0);
         
         for (auto it: prerequisites) {
             adj[it[1]].push_back(it[0]);
         }
         for (int i = 0; i < numCourses; i++) {
-            if (is_cycle(visited, i))
+            if (is_cycle(visites, i))
                 return false;
         }
         return true;
     }
 };
-
-/*
-2
-[[1,1]]
-2
-[[1,0]]
-2
-[[1,0],[0,1]]
-4
-[[1,0],[2,1],[2,0],[3,2]]
-4
-[[0,2],[2,1],[1,0],[3,2]]
-5
-[[2,0],[0,1],[1,2],[4,3],[0,4]]
-
-*/

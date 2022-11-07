9f2d8b452959dcce86668ffa76d4147469c6b391
JI-HUN KIM <jihuun.k@gmail.com>
2022-11-07 21:25:48 +0900

    Time: 57 ms (26.18%), Space: 14.8 MB (22.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,34 @@
 class Solution {
     vector<vector<int>> adj;
-    bool is_cycle(vector<int> &vis, int cur) {
-        /*
-        vis 0: not visited
-        vis 1: visited
-        vis 2: no need to visit
-        */
-        if (vis[cur] == 1)
-            return true;
-        if (vis[cur] == 0) {
-            vis[cur] = 1;
-            for (int i = 0; i < adj[cur].size(); i++) {
-                if (is_cycle(vis, adj[cur][i]))
-                    return true;
-            }
-        }
-        vis[cur] = 2;
-        return false;
-    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         adj = vector<vector<int>>(numCourses);
-        vector<int> visites(numCourses, 0);
-        
+        unordered_map<int, int> indeg;
+        vector<int> ret;
+        // [1] -> [0]
         for (auto it: prerequisites) {
             adj[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
         }
+        
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (is_cycle(visites, i))
-                return false;
+            if (indeg[i] == 0)
+                q.push(i);
         }
-        return true;
+        while (!q.empty()) {
+            int tgt = q.front();
+            q.pop();
+            ret.push_back(tgt);
+            for (int i = 0; i < adj[tgt].size(); i++) {
+                int adjval = adj[tgt][i];
+                indeg[adjval]--;
+                if (indeg[adjval] == 0)
+                    q.push(adjval);
+            }
+        }
+        if (ret.size() == numCourses)
+            return true;
+        return false;
     }
 };

ce84f3104755ccf0f517d43bfc4984b061d15273
JI-HUN KIM <jihuun.k@gmail.com>
2022-11-18 12:55:50 +0900

    Time: 10 ms (46.63%), Space: 10.2 MB (9.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,27 @@
 class Solution {
+    bool is_not_common(vector<string> strs, int idx) {
+        string cmps = strs[0].substr(0, idx + 1);
+        for (int i = 0; i < strs.size(); i++) {
+            if (strs[i].substr(0, idx + 1) != cmps)
+                return true;
+        }
+        return false;
+    }
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int idx = 0;
-        string longest = "";
-        
-        while (1) {
-            for (int i = 0; i < strs.size(); i++) {
-                if (strs[i][idx] == '\0' || strs[i][idx] != strs[0][idx])
-                    return longest;
-            }
-            longest = longest + strs[0][idx];
-            idx++;
+        int left = 0;
+        int right = strs[0].length();
+        for (int i = 1; i < strs.size(); i++) {
+            right = std::min(right, (int)strs[i].length());
         }
-        return longest;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (is_not_common(strs, mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return strs[0].substr(0, left);
     }
 };

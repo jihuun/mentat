2badd4122bb26b828e1599bbef2bdc40823d1a12
JI-HUN KIM <jihuun.k@gmail.com>
2022-12-02 11:37:11 +0900

    Time: 252 ms (11.06%), Space: 46.6 MB (22.60%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         unordered_map<int, int> table;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0)
-                table[nums[i]]++;
-        }
         int maxval = 0;
-        for (auto it: table) {
-            maxval = max(maxval, it.first);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                table[nums[i]]++;
+                maxval = max(maxval, nums[i]);
+            }
         }
         for (int i = 1; i <= maxval; i++) {
             if (table.find(i) == table.end())

250194994fdabcce8c5c274ea2cffa693e2f4783
JI-HUN KIM <jihuun.k@gmail.com>
2022-12-27 21:26:01 +0900

    Time: 116 ms (7.24%), Space: 221.2 MB (5.10%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*
+3 9 20 15 7     preorder
+^ l rrrrrrr
+
+9 3 15 20 7     inorder
+l | rrrrrrr
+  |
+  pivot
+  
+*/
+class Solution {
+public:
+    TreeNode *build_tree(vector<int> pre, vector<int> in, int left, int right, int root_idx) {
+        // left, right -> index of in
+        // root_idx -> index of pre
+        if (left > right) /* base case */
+            return NULL;
+        int pivot = left;
+        for (pivot = left; pivot <= right; pivot++)
+            if (in[pivot] == pre[root_idx])
+                break;
+            
+        TreeNode *node = new TreeNode;
+        node->val = pre[root_idx];
+        node->left = build_tree(pre, in, left, pivot - 1, root_idx + 1);
+        node->right = build_tree(pre, in, pivot + 1, right, root_idx + (pivot - left + 1));
+        return node;
+    }
+    
+    TreeNode *buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return build_tree(preorder, inorder, 0, inorder.size() - 1, 0);
+    }
+};

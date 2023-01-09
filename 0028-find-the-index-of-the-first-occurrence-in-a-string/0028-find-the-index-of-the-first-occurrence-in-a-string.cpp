c480209962b16f6e63e34da69a672b147c4392ec
JI-HUN KIM <jihuun.k@gmail.com>
2023-01-09 20:17:32 +0900

    Time: 4 ms (37.28%), Space: 6.5 MB (11.82%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        std::hash<string> str_hash;
+        size_t hval_needle = str_hash(needle);
         int nsize = needle.length();
-        for (int i = 0; i < haystack.length(); i++) {
-            if (haystack.substr(i, nsize) == needle)
+        // abcd (cd)
+        
+        for (int i = 0; i + nsize - 1 < haystack.length(); i++) {
+            if (str_hash(haystack.substr(i, nsize)) == hval_needle)
                 return i;
         }
         return -1;

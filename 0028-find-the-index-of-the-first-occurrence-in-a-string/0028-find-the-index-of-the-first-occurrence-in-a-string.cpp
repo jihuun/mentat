099829ea4bc6c7ef8be2bf4f74e703298d08b3cf
JI-HUN KIM <jihuun.k@gmail.com>
2023-01-10 22:29:34 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (11.85%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
+    unsigned int make_hash(string s, int st, int end) {
+        unsigned int sum = 0;
+        for (int i = st; i < end; i++) {
+            sum += (unsigned int)s[i];
+        }
+        return sum;
+    }
+    bool double_check(string s1, string s2, int st, int end) {
+        int idx = 0;
+        for (int i = st; i < end; i++) {
+            if (s1[i] != s2[idx++])
+                return false;
+        }
+        return true;
+    }
     int strStr(string haystack, string needle) {
-        std::hash<string> str_hash;
-        size_t hval_needle = str_hash(needle);
-        int nsize = needle.length();
-        // abcd (cd)
+        int nsize = needle.size();
+        unsigned int hval_needle = make_hash(needle, 0, nsize);
+        unsigned int hval_window = make_hash(haystack, 0, nsize);
+        //cout << hval_needle << endl;
         
-        for (int i = 0; i + nsize - 1 < haystack.length(); i++) {
-            if (str_hash(haystack.substr(i, nsize)) == hval_needle)
+        if (hval_window == hval_needle && double_check(haystack, needle, 0, nsize))
+            return 0;
+        for (int i = 1; i + nsize <= haystack.length(); i++) {
+            hval_window -= (unsigned int)haystack[i - 1];
+            hval_window += (unsigned int)haystack[i + nsize - 1];
+            //cout << hval_window << " "; 
+            if (hval_window == hval_needle && double_check(haystack, needle, i, i + nsize))
                 return i;
         }
         return -1;

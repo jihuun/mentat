f956cacddd78056ead53ece722bad921a78aa857
JI-HUN KIM <jihuun.k@gmail.com>
2023-01-26 22:37:25 +0900

    Time: 32 ms (42.72%), Space: 14.7 MB (29.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
+    /*
+[1,0] [2,0] [3,1] [3,2] 로 주어질때
+
+이를 그래프 자료구조로 표현하면
+[0] -> 1 -> 2
+[1] -> 3
+[2] -> 3
+
+진입차수(indegree)를 표현하면
+[0] 0
+[1] 1
+[2] 1
+[3] 2
+    */
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
         unordered_map<int, int> indeg;
         vector<int> ret;
+        queue<int> q;
         
         for (auto it: prerequisites) {
             graph[it[1]].push_back(it[0]);
             indeg[it[0]]++;
         }
-        queue<int> q;
+        
         for (int i = 0; i < numCourses; i++) {
             if (indeg[i] == 0)
                 q.push(i);
         }
+        
         while (!q.empty()) {
-            int tgt = q.front();
+            int n = q.front();
             q.pop();
-            ret.push_back(tgt);
-            for (int i = 0; i < graph[tgt].size(); i++) {
-                int adj = graph[tgt][i];
-                indeg[adj]--;
-                if (indeg[adj] == 0)
-                    q.push(adj);
+            ret.push_back(n);
+            for (int i = 0; i < graph[n].size(); i++) {
+                indeg[graph[n][i]]--;
+                if (indeg[graph[n][i]] == 0)
+                    q.push(graph[n][i]);
             }
         }
-        if (ret.size() == numCourses)
+        if (ret.size() == numCourses)    
             return ret;
         return {};
     }

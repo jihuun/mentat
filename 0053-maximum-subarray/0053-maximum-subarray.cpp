c3b0bb97c1e2a875891dc198e146f0268463f80f
JI-HUN KIM <jihuun.k@gmail.com>
2023-02-05 22:38:27 +0900

    Time: 166 ms (21.94%), Space: 67.8 MB (60.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxsub(vector<int>& nums, int left, int right) {
+        if (left > right)
+            return 0;
+        if (left == right)
+            return nums[left];
+        int mid = left + (right - left) / 2;
+        int l_sum = nums[mid];
         int sum = 0;
-        int maxval = nums[0];
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = mid; i >= left; i--) {
             sum += nums[i];
-            maxval = std::max(maxval, sum);
-            if (sum < 0)
-                sum = 0;
+            l_sum = std::max(l_sum, sum);
         }
-        return maxval;
+        int r_sum = nums[mid+1];
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            r_sum = std::max(r_sum, sum);
+        }
+        int left_half = maxsub(nums, left, mid);
+        int right_half = maxsub(nums, mid + 1, right);
+        return std::max(l_sum + r_sum, std::max(left_half, right_half));
+    }
+    int maxSubArray(vector<int>& nums) {
+        return maxsub(nums, 0, nums.size() - 1);
     }
 };

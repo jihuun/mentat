611acf5c188bb26fdc91f008d7dab51b59a5b4f6
JI-HUN KIM <jihuun.k@gmail.com>
2023-02-05 22:13:22 +0900

    Time: 121 ms (65.71%), Space: 67.7 MB (60.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
-    int maxarr(vector<int> &nums, int left, int right) {
-        if (left > right)
-            return 0;
-        if (left == right)
-            return nums[left];
-        
-        /* find max sum expanding from mid to both side*/
-        int mid = left + (right - left) / 2;
-        int max_l = nums[mid];
-        int l_sum = 0;
-        for (int i = mid; i >= left; i--) { // [left, mid] must check the range
-            l_sum += nums[i];
-            max_l = std::max(max_l, l_sum);
-        }
-        int max_r = nums[mid + 1];
-        int r_sum = 0;
-        for (int i = mid + 1; i <= right; i++) { // [mid+1, right]
-            r_sum += nums[i];
-            max_r = std::max(max_r, r_sum);
-        }
-        
-        /* compare three values */
-        int lefthalf = maxarr(nums, left, mid);
-        int righthalf = maxarr(nums, mid + 1, right);
-        return std::max(max_l + max_r, std::max(lefthalf, righthalf));
-    }
-    
     int maxSubArray(vector<int>& nums) {
-        return maxarr(nums, 0, nums.size() - 1);
+        int sum = 0;
+        int maxval = nums[0];
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            maxval = std::max(maxval, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        return maxval;
     }
 };

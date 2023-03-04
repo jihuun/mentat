5324e77cc392b7162531e6b3bd24f16429712269
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-04 22:14:38 +0900

    Time: 322 ms (59.39%), Space: 10.5 MB (57.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,16 @@
 class Solution {
-    int binary_search(vector<int> &nums, int tgt) {
-        int left = 0;
-        int right = nums.size() - 1;
-        //  1 2 3 5  (4)
-        // 1 3 5 7  (2)
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == tgt) {
-                return mid;
-            } else if (nums[mid] < tgt) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> sub;
-        sub.push_back(nums[0]);
+        vector<int> mem(nums.size(), 1);
+        int maxlen = 1;
+        
         for (int i = 0; i < nums.size(); i++) {
-            if (sub[sub.size()-1] < nums[i]) {
-                sub.push_back(nums[i]);
-            } else {
-                int j = binary_search(sub, nums[i]);
-                sub[j] = nums[i];
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i])
+                    mem[i] = std::max(mem[i], mem[j] + 1);
             }
+            maxlen = std::max(maxlen, mem[i]);
         }
-        return sub.size();
+        return maxlen;
     }
 };

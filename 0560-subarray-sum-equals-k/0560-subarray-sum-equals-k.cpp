ce2fb9f54d3efe60f1b55579ca16abcf5ccf0f53
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-05 22:17:54 +0900

    Time: 88 ms (86.84%), Space: 41.4 MB (85.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        int cur_sum = 0;
+        unordered_map<int, int> table;
+        int sum = 0;
         int cnt = 0;
         
-        freq[0]++; // for case of (cur_sum - k == 0)
+        table[0] = 1;
         for (int i = 0; i < nums.size(); i++) {
-            cur_sum += nums[i];
-            
-            if (freq.find(cur_sum - k) != freq.end()) {
-                cnt += freq[cur_sum - k];
+            sum += nums[i];
+            if (table.find(sum - k) != table.end()) {
+                cnt += table[sum - k];
             }
-            freq[cur_sum]++;
+            table[sum]++;
         }
         return cnt;
     }

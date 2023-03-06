8f598146c8d0fc3d430e2dfa81ee6bb136aca3ee
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-06 21:25:17 +0900

    Time: 114 ms (5.08%), Space: 10.7 MB (81.54%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,35 @@
 class Solution {
-    vector<vector<int>> mem;
-    string t1;
-    string t2;
+public:
+    int mem[1001][1001];
+    string s1, s2;
     
-    int longest(int i1, int i2) {
+    int recur(int i1, int i2) {
         if (i1 < 0 || i2 < 0)
             return 0;
+        if (i1 == 0 && i2 == 0) {
+            if (s1[i1] == s2[i2])
+                return 1;
+            else
+                return 0;
+        }
         if (mem[i1][i2] != -1)
             return mem[i1][i2];
-        if (t1[i1] == t2[i2]) {
-            mem[i1][i2] = longest(i1 - 1, i2 - 1) + 1;
+        
+        if (s1[i1] == s2[i2]) {
+            mem[i1][i2] = recur(i1-1, i2-1) + 1;
         } else {
-            mem[i1][i2] = max(longest(i1, i2 - 1), longest(i1 - 1, i2));
+            mem[i1][i2] = std::max(recur(i1, i2-1), recur(i1-1, i2));
         }
         return mem[i1][i2];
-        
+            
     }
-public:
     int longestCommonSubsequence(string text1, string text2) {
-        mem = vector<vector<int>>(1001, vector<int>(1001, -1));
-        t1 = text1;
-        t2 = text2;
-        return longest(t1.size() - 1, t2.size() - 1);
+        s1 = text1;
+        s2 = text2;
+        for (int i = 0; i < 1001; i++)
+            for (int j = 0; j < 1001; j++)
+                mem[i][j] = -1;
+        
+        return recur(s1.size() -1, s2.size() -1);
     }
 };

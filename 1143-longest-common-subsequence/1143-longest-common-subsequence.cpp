d8ff4f2f37666109d22f03d34c1687ef5793b48f
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-08 21:48:45 +0900

    Time: 33 ms (74.29%), Space: 10.6 MB (81.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int mem[1001][1001];
-    string s1, s2;
-    
-    int recur(int i1, int i2) {
-        if (i1 < 0 || i2 < 0)
-            return 0;
-        if (i1 == 0 && i2 == 0) {
-            if (s1[i1] == s2[i2])
-                return 1;
-            else
-                return 0;
-        }
-        if (mem[i1][i2] != -1)
-            return mem[i1][i2];
-        
-        if (s1[i1] == s2[i2]) {
-            mem[i1][i2] = recur(i1-1, i2-1) + 1;
-        } else {
-            mem[i1][i2] = std::max(recur(i1, i2-1), recur(i1-1, i2));
-        }
-        return mem[i1][i2];
-            
-    }
+    string t1, t2;
     int longestCommonSubsequence(string text1, string text2) {
-        s1 = text1;
-        s2 = text2;
-        for (int i = 0; i < 1001; i++)
-            for (int j = 0; j < 1001; j++)
-                mem[i][j] = -1;
-        
-        return recur(s1.size() -1, s2.size() -1);
+        int mem[1002][1002] = {0};
+        t1 = text1;
+        t2 = text2;
+        for (int i = t1.size() - 1; i >= 0; i--) {
+            for (int j = t2.size() - 1; j >= 0; j--) {
+                if (t1[i] == t2[j]) {
+                    mem[i][j] = mem[i+1][j+1] + 1;
+                } else {
+                    mem[i][j] = std::max(mem[i+1][j], mem[i][j+1]);
+                }
+            }
+        }
+        return mem[0][0];
     }
 };

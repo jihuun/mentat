047a12383a3365e8f24b15f094a30af52b0f00ad
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-10 21:13:01 +0900

    Time: 318 ms (59.46%), Space: 10.7 MB (38.58%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> mem(nums.size(), 1);
-        int maxlen = 1;
-        
+        int longest = 1;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i])
+                if (nums[j] < nums[i]) {
                     mem[i] = std::max(mem[i], mem[j] + 1);
+                }
             }
-            maxlen = std::max(maxlen, mem[i]);
+            longest = std::max(longest, mem[i]);
         }
-        return maxlen;
+        return longest;
     }
 };

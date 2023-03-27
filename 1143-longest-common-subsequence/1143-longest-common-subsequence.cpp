d96688e041dc154c20e0842da19757b385f24589
JI-HUN KIM <jihuun.k@gmail.com>
2023-03-27 20:02:03 +0900

    Time: 83 ms (16.32%), Space: 24.4 MB (22.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,15 +4,16 @@ public:
         int size1 = text1.size();
         int size2 = text2.size();
         vector<vector<int>> mem(size1 + 1, vector<int>(size2 + 1, 0));
-
+        
         for (int i = size1 - 1; i >= 0; i--) {
             for (int j = size2 - 1; j >= 0; j--) {
-                if (text1[i] == text2[j])
+                if (text1[i] == text2[j]) {
                     mem[i][j] = mem[i+1][j+1] + 1;
-                else
+                } else {
                     mem[i][j] = std::max(mem[i+1][j], mem[i][j+1]);
+                }
             }
-        }   
+        }
         return mem[0][0];
     }
 };

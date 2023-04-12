8119814fa80d22bf5c532dd5fdef2bdc7c9d7522
JI-HUN KIM <jihuun.k@gmail.com>
2023-04-12 21:42:06 +0900

    Time: 3 ms (45.69%), Space: 6 MB (8.25%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+// 1 2 3 (4) 5 
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+};

b0bb8a51fe9bb09bc6159f355395a31f0faa3f04
JI-HUN KIM <jihuun.k@gmail.com>
2023-08-18 21:20:41 +0900

    Time: 16 ms (67.38%), Space: 8.5 MB (57.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    std::unordered_map<char, int> table;
+    unordered_map<char, int> pos;
     int l = 0, r = 0;
     int maxlen = 0;
 
     int lengthOfLongestSubstring(std::string s) {
-        while (r < s.length()) {
-            table[s[r]]++;
-            while (table[s[r]] > 1) {
-                table[s[l]]--;
-                l++;
-            }
-            maxlen = std::max(maxlen, r - l + 1);
-            r++;
+        for (int r = 0; r < s.length(); r++) {
+            if (pos.find(s[r]) != pos.end()) // abba
+                l = max(l, pos[s[r]] + 1);
+            pos[s[r]] = r;
+            maxlen = max(maxlen, r - l + 1);
         }
         return maxlen;
     }

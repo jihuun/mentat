781fb0ca9c39afc033b3820d57c0b335a4172c64
JI-HUN KIM <jihuun.k@gmail.com>
2023-09-21 22:28:58 +0900

    Time: 5 ms (82.43%), Space: 10.2 MB (10.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> mem;
-    int minPathSum(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        mem = vector(row + 1, vector<int>(col + 1, -1));
-        return minsum(grid, 0, 0);
-    }
-    
-    int minsum(vector<vector<int>>& grid, int i, int j) {
-        if (mem[i][j] != -1)
-            return mem[i][j];
-        if (i == grid.size() || j == grid[0].size())
+    int vis[201][201] = {0};
+    int minsum(vector<vector<int>>& grid, int i , int j) {
+        if (i < 0 || j < 0)
             return INT_MAX;
-        if (i == grid.size() - 1 && j == grid[0].size() - 1)
+        if (i == 0 && j == 0)
             return grid[i][j];
+        if (vis[i][j])
+            return vis[i][j];
         
-        mem[i][j] = grid[i][j] + min(minsum(grid, i+1, j), minsum(grid, i, j+1));
-        return mem[i][j];
+        vis[i][j] = grid[i][j] + std::min(minsum(grid, i - 1, j), minsum(grid, i, j - 1));
+        return vis[i][j];
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        return minsum(grid, grid.size() - 1, grid[0].size() - 1);
     }
 };

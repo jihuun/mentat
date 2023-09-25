0d6b03bfd508c27020f7bb1e0326b418e826e6af
JI-HUN KIM <jihuun.k@gmail.com>
2023-09-25 22:50:10 +0900

    Time: 11 ms (24.34%), Space: 10 MB (76.49%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int vis[201][201] = {0};
-    int minsum(vector<vector<int>>& grid, int i , int j) {
+    int minsum(vector<vector<int>>& grid, int i, int j) {
         if (i < 0 || j < 0)
             return INT_MAX;
         if (i == 0 && j == 0)
             return grid[i][j];
         if (vis[i][j])
-            return vis[i][j];
-        
-        vis[i][j] = grid[i][j] + std::min(minsum(grid, i - 1, j), minsum(grid, i, j - 1));
+           return vis[i][j];
+        vis[i][j] = grid[i][j] + min(minsum(grid, i - 1, j), minsum(grid, i, j-1));
         return vis[i][j];
     }
     int minPathSum(vector<vector<int>>& grid) {

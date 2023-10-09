10624db27b7b5b1a9234ff86ca33c93de44a6745
JI-HUN KIM <jihuun.k@gmail.com>
2023-10-09 22:02:26 +0900

    Time: 0 ms (100.00%), Space: 8 MB (35.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> vis;
-    int act(vector<int>& nums, int n) {
-        if (n < 0)
+    int act(vector<int>& nums, int h) { 
+        if (h < 0)
             return 0;
-        if (vis[n] != -1)
-            return vis[n];
-        vis[n] = max(act(nums, n-1), act(nums, n -2) + nums[n]);
-        return vis[n];
+        if (vis[h] != -1)
+            return vis[h];
+        vis[h] = max(act(nums, h-2) + nums[h], act(nums, h-1));
+        return vis[h];
     }
-    
     int rob(vector<int>& nums) {
         vis = vector<int>(101, -1);
         return act(nums, nums.size() - 1);

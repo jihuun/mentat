b37d9e5f661e4064bd3b8e0910effcee61f92d3c
JI-HUN KIM <jihuun.k@gmail.com>
2023-10-10 21:42:06 +0900

    Time: 0 ms (100.00%), Space: 8.3 MB (21.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,36 @@
 class Solution {
-public:
-    vector<int> mem;
-    int max_rob(vector<int> nums, int begin, int idx) {
-        if (idx < begin)
-            return 0;
-        if (mem[idx] != -1)
-            return mem[idx];
-        mem[idx] = std::max(max_rob(nums, begin, idx - 1), max_rob(nums, begin, idx - 2) + nums[idx]);
-        return mem[idx];
-    }
+    /*
+     1 2 3 4 
+     2 3 4 5
+     
+         1 
+      2      5
+        3  4  
     
+    */
+public:
+    int act(vector<int>& vis, vector<int>& nums, int h) {
+        if (h < 0)
+            return 0;
+        if (vis[h] != -1)
+            return vis[h];
+        vis[h] = max(act(vis, nums, h - 1), act(vis, nums, h - 2) + nums[h]);
+        return vis[h];
+    }
     int rob(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-
-        // 1 (2 3 4 5)
-        mem = vector<int>(nums.size(), -1);
-        int val1 = max_rob(nums, 1, nums.size() - 1);
+        vector<int> n1, n2;
+        vector<int> vis1(101, -1), vis2(101, -1);
         
-        // (1 2 3 4) 5
-        mem = vector<int>(nums.size(), -1);
-        int val2 = max_rob(nums, 0, nums.size() - 2);
-        
-        return std::max(val1, val2);
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0) {
+                n2.push_back(nums[i]);
+            } else if (i == nums.size() - 1) {
+                n1.push_back(nums[i]);
+            } else {
+                n1.push_back(nums[i]);
+                n2.push_back(nums[i]);
+            }
+        }
+        return max(act(vis1, n1, n1.size() - 1), act(vis2, n2, n2.size() - 1)); 
     }
 };
